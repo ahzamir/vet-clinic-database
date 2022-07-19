ebfc5a1cc89da72332eab023330b1c89393c7096
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-07-19 17:36:37 +0430

    created the vet-clinic database

--- schema.sql
+++ schema.sql
@@ -1,5 +1,8 @@
 /* Database schema to keep the structure of entire database. */
 
+/* I have Created the Database here. */
+postgres=# CREATE DATABASE vet_clinic;
+
 CREATE TABLE animals (
     name varchar(100)
 );
