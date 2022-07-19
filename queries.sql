ec482b53bde1b6bc065c9f2141c54e03c72eac4e
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-07-19 20:09:45 +0430

    select date of birth of animals named either 'Agumon' or 'Pikachu'

--- queries.sql
+++ queries.sql
@@ -2,3 +2,5 @@
 
 SELECT * FROM animals WHERE name LIKE '%mon%';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' and '2019-12-31';
+SELECT name FROM animals WHERE neutered = '1' and escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
