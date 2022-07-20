d727241864b2fe8b8d68eacbf40edd834accc578
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-07-20 16:46:37 +0430

    select the number of animals in the table

--- queries.sql
+++ queries.sql
@@ -7,4 +7,5 @@ SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 SELECT (name, escape_attempts) FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = '1';
 SELECT * FROM animals WHERE name != 'Gabumon';
-SELECT * FROM animals WHERE weight_kg >= 10.4 AND  weight_kg <= 17.3;
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND  weight_kg <= 17.3
+SELECT COUNT(*) FROM animals;
