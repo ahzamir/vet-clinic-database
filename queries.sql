127fb61b5094aa3c45d0c18fb8be7e05a2ef48fc
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-07-22 17:09:11 +0430

    write a query for who was Maisy Smith's first visit

--- queries.sql
+++ queries.sql
@@ -27,3 +27,4 @@ SELECT COUNT(animals.name) FROM animals JOIN visits ON animals_id = animals.id W
 SELECT vets.name, species.name FROM species RIGHT JOIN specializations ON species.id = specializations.species_id RIGHT JOIN vets ON vets.id = specializations.vets_id;
 SELECT animals.name FROM animals JOIN visits ON animals.id = visits.animals_id JOIN vets ON vets.id = vets_id WHERE vets.id = 3 AND visits.date BETWEEN '2020-04-01' AND '2020-08-30';
 SELECT COUNT(animals.name) AS total, animals.name FROM animals JOIN visits ON animals.id = visits.animals_id JOIN vets ON vets.id = visits.vets_id GROUP BY animals.name ORDER BY total DESC LIMIT 1;
+SELECT animals.name, date FROM animals JOIN visits ON animals.id = visits.animals_id JOIN vets ON vets.id = visits.vets_id WHERE vets.id = 2 ORDER BY date LIMIT 1;
